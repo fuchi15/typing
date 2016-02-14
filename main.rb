bcaa90c0334cc12f3a9464edf6be8c66e644c8f8
fuchi15 <sakuya815@gmail.com>
2016-02-14 22:18:41 +0900

    完成

--- main.rb
+++ main.rb
@@ -1,10 +1,28 @@
+
 require 'json'
 
-# とりあえず変数fileに展開する
-question=open('question.json')
-
-# fileを1行読む毎にパースして、その上で出力する
-question.each_line { |line|
-    json=JSON.load(line)
-    puts json['question']
+json=open('question.json'){|io|
+ JSON.load(io)
 }
+start_time = Time.now
+score = 0
+
+json.each do |key,val|
+
+  puts "問題: #{val}\n"
+  a = gets.chomp
+  if /#{val}/ =~ a
+	puts "正解"
+  end
+  if(val.length < 5)  	
+  	score += 100
+  elsif(val.length <= 5 && val.length > 10)
+  	score += 200
+  else
+  	score += 300
+  end
+
+end
+
+puts "時間 #{Time.now - start_time}s"
+puts "#{score}"
